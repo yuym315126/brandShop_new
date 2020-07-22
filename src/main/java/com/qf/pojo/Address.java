24d67113ff93598dfd3bca6f313dfeeffc4b06a4
muziLee226 <1797063935@qq.com>
2020-07-22 21:47:14 +0800

    7/22 21：46 ll 地址

---
+++ src/main/java/com/qf/pojo/Address.java
@@ -0,0 +1,24 @@
+package com.qf.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @PROJECT_NAME: brandshop
+ * @DESCRIPTION:
+ * @USER: 木子Lee
+ * @DATE: 2020/7/21 19:23
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    private Integer addressId;
+    private String addressee;
+    private String addPhone;
+    private String area;
+    private String street;
+    private Integer userId;
+}
